f8a254a07183f2b9f7d1c95c97e45a03ed229737
DingZY <331646081@qq.com>
2022-12-08 08:16:00 +0800

    add exercises

--- C语言程序设计（乐学）/【大学】撞球+/1.c
+++ C语言程序设计（乐学）/【大学】撞球+/1.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 int main() {
-    double w = 0, l = 0, x0 = 0, y0 = 0, x1 = 0, y1 = 0;
-    char str[1200] = "0";
-    scanf("%lf %lf", &w, &l);
-    scanf("%lf %lf", &x0, &y0);
-    scanf("%lf %lf", &x1, &y1);
-    scanf("%s", str);
-    int i = 0;
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] == 'F')
-            y0 = -y0;
-        else if (str[i] == 'B')
-            y0 = 2 * l - y0;
-        else if (str[i] == 'L')
-            x0 = -x0;
-        else if (str[i] == 'R')
-            x0 = 2 * w - x0;
+    double w, l, x0, y0, x1, y1;
+    char str[1200];
+    scanf("%lf%lf%lf%lf%lf%lf%s", &w, &l, &x0, &y0, &x1, &y1, str);
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] == 'F')y0 = -y0;
+        else if (str[i] == 'B')y0 = 2 * l - y0;
+        else if (str[i] == 'L')x0 = -x0;
+        else if (str[i] == 'R')x0 = 2 * w - x0;
     }
-    double x = 0, y = 0, temp = 0, s = 0;
-    x = x1 - x0;
-    y = y1 - y0;
-    temp = x * x + y * y;
-    s = sqrt(temp);
-    printf("%.4lf\n", s);
+    double x = x1 - x0, y = y1 - y0;
+    printf("%.4lf\n", sqrt(x * x + y * y));
 }
